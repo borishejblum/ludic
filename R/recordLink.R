a49f59dc59740b75fc070ae175fd3f57de5ee143
Boris <boris.hejblum.ensai@gmail.com>
2016-12-28 16:50:59 +0100

    adding an example

--- R/recordLink.R
+++ R/recordLink.R
@@ -6,11 +6,11 @@
 #'
 #'@param data1_cont2diff either a matrix or dataframe of continuous features, 
 #'such as age, for which the similarity measure uses the difference with 
-#'\code{data2_cont2diff}, whose rownames are .
+#'\code{data2_cont2diff}, whose rownames are . Default is \code{NULL}.
 #'
 #'@param data2_cont2diff either a matrix or dataframe of continuous features, 
 #'such as age, for which the similarity measure uses the difference with 
-#'\code{data2_cont1diff}, whose rownames are .
+#'\code{data2_cont1diff}, whose rownames are . Default is \code{NULL}.
 #'
 #'@param eps_plus discrepancy rate between \code{data1} and \code{data2}
 #'
@@ -47,12 +47,27 @@
 #'
 #'@return the posterior probability of matching matrix
 #'
+#'@examples
+#'set.seed(123)
+#'ncodes <- 500
+#'npat <- 200
+#'incid <- abs(rnorm(n=ncodes, 0.15, 0.07))
+#'bin_codes <- rbinom(n=npat*ncodes, size=1,  prob=rep(incid, npat))
+#'bin_codes_mat <- matrix(bin_codes, ncol=ncodes, byrow = TRUE)
+#'data1_ex <- bin_codes_mat[1:(npat/2+npat/10),]
+#'data2_ex <- bin_codes_mat[c(1:(npat/10), (npat/2+npat/10 + 1):npat),]
+#'rownames(data1_ex) <- paste0("ID", 1:(npat/2+npat/10), "_data1")
+#'rownames(data2_ex) <- paste0("ID", c(1:(npat/10), (npat/2+npat/10 + 1):npat), "_data2")
+#'
+#'res <- recordLink(data1 = data1_ex, data2 = data2_ex, use_diff=FALSE, eps_minus = 0.01, eps_plus = 0.01)
+#'round(res[c(1:3, 20:23), c(1:3, 20:23)], 3)
+#'
 #'@export
 
 recordLink <- function(data1, data2, dates1 = NULL, dates2 = NULL,
                        eps_plus, eps_minus, aggreg_2ways = "mean", 
                        min_prev = 0.01, 
-                       data1_cont2diff, data2_cont2diff,
+                       data1_cont2diff = NULL, data2_cont2diff = NULL,
                        #eps_inf, 
                        d_max, use_diff=TRUE){
   
