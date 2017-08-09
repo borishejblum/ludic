b2b4479eec1a0981d16aef1c9290ff53174c51ae
Boris <boris.hejblum.ensai@gmail.com>
2017-08-09 11:54:10 +0200

    Starting to use testthat

---
+++ tests/testthat/test_pval_zscore.R
@@ -0,0 +1,8 @@
+library(ludic)
+test_that("-1.96 and 1.96 both have p-value 0.05", {
+  expect_equal(round(pval_zscore(1.96, 1), 4), 0.05)
+  expect_equal(round(pval_zscore(-1.96, 1), 4), 0.05)
+  
+})
+
+
